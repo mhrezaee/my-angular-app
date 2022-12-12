5d2d5bde1987de8705298c657b9f62020ebc5c27
Hadi <se.hadirezaee@gmail.com>
2022-12-12 18:37:16 +0100

    show some domy data on list

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -4,4 +4,28 @@ import { Component } from '@angular/core';
   selector: 'pm-products',
   templateUrl: './product-list.component.html',
 })
-export class ProductListComponent {}
+export class ProductListComponent {
+  pageTitle: string = 'Products';
+  products: any[] = [
+    {
+      id: 3,
+      name: 'Monitor',
+      code: 'PC-003',
+      releaseDate: '20.12.2020',
+      description: '24 inch monitor!',
+      price: 150.99,
+      rate: 4.2,
+      imageUrl: 'assets/images/monitor.png',
+    },
+    {
+      id: 7,
+      name: 'Keyboard',
+      code: 'PC-007',
+      releaseDate: '15.08.2021',
+      description: 'a very nice keyboard!',
+      price: 81.99,
+      rate: 4.8,
+      imageUrl: 'assets/images/keyboard.png',
+    },
+  ];
+}
