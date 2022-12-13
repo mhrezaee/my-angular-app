b3836ff0f860822b7cdea2594a1e0d4aa03e8825
Hadi <se.hadirezaee@gmail.com>
2022-12-13 18:48:02 +0100

    add product interface

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { IProduct } from './product';
 
 @Component({
   selector: 'pm-products',
@@ -10,7 +11,7 @@ export class ProductListComponent {
   imageMargin: number = 2;
   showImage: boolean = false;
   searchTerm: string = '';
-  products: any[] = [
+  products: IProduct[] = [
     {
       id: 3,
       name: 'Monitor',
