f2e9e95968380221239f8c3f3f737cee485fb9cc
Hadi <se.hadirezaee@gmail.com>
2022-12-25 12:44:56 +0100

    welcome component

--- src/app/products/product-detail.component.ts
+++ src/app/products/product-detail.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'pm-product-detail',
   templateUrl: './product-detail.component.html',
   styleUrls: ['./product-detail.component.css']
 })
-export class ProductDetailComponent {
+export class ProductDetailComponent implements OnInit {
+constructor(){
 
 }
+
+pageTitle : string = 'Product Detail'
+
+ngOnInit(): void {
+  
+}
+
+}
+
