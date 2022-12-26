56db98d77f15afd1f82b7c26dfea9bcf1121f115
Hadi <se.hadirezaee@gmail.com>
2022-12-26 19:30:35 +0100

    update product detail page with routing

--- src/app/products/product-detail.component.ts
+++ src/app/products/product-detail.component.ts
@@ -1,19 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { IProduct } from './product';
 
 @Component({
   templateUrl: './product-detail.component.html',
-  styleUrls: ['./product-detail.component.css']
+  styleUrls: ['./product-detail.component.css'],
 })
 export class ProductDetailComponent implements OnInit {
-constructor(){
+  constructor(private _route: ActivatedRoute) {}
 
+  pageTitle: string = 'Product Detail';
+  product: IProduct | undefined;
+
+  ngOnInit(): void {
+    const id = Number(this._route.snapshot.paramMap.get('id'));
+    this.pageTitle += `: ${id}`;
+    this.product = {
+      id: id,
+      name: 'Monitor',
+      code: 'PC-003',
+      releaseDate: '20.12.2020',
+      description: '24 inch monitor!',
+      price: 150.99,
+      rate: 4.2,
+      imageUrl: 'assets/images/monitor.png',
+    };
+  }
 }
-
-pageTitle : string = 'Product Detail'
-
-ngOnInit(): void {
-  
-}
-
-}
-
